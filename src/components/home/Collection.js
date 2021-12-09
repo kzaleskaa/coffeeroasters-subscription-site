7892632ef483b027c52d9e3ce22c80fcada6e4ab
Kasia Zaleska <kzaleska416@gmail.com>
2021-12-09 18:47:00 +0100

    add style to collection

--- src/components/home/Collection.js
+++ src/components/home/Collection.js
@@ -1,14 +1,20 @@
+import styles from "./Collection.module.css";
+
 const Collection = (props) => {
   return (
-    <section>
+    <section className={styles.collection}>
       <h2>our collection</h2>
-      {props.data.map((product) => (
-        <div key={product.key}>
-          <img src={product.img} alt={product.name} />
-          <h3>{product.name}</h3>
-          <p>{product.about}</p>
-        </div>
-      ))}
+      <div className={styles.products}>
+        {props.data.map((product) => (
+          <div key={product.key} className={styles.product}>
+            <img src={product.img} alt={product.name} />
+            <div className={styles.text}>
+              <h3>{product.name}</h3>
+              <p>{product.about}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
